b61e14efc2f35da983c24e76ef75a63314eb84cd
Peter Le <Peterle3@csus.edu>
2017-10-07 04:49:41 -0700

    sends the text back to main.js page

--- app/static/main.js
+++ app/static/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-$("#submit").click(function() {
-	alert($("#input").val());
+	$("#submit").click(function() {
+      var text = $("#input").val();
+	  $.ajax({url: 'http://localhost:5000/test', 
+	    type : 'POST',
+	    data: {
+	    	key: text
+	    },
+	     success: function(res){
+	       console.log("success!", res.key);
+	      	
+	       $("#test").text(res.key[0]);
+	     }
+	      
+		})
 	})
 })
