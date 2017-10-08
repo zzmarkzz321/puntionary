e5fba79676c83433069ff2368f8ce4d285d71ea2
Cindy <cindynhoang@gmail.com>
2017-10-08 02:42:02 -0700

    Add animations and result text field

--- mobile/App.js
+++ mobile/App.js
@@ -1,51 +1,74 @@
 import React, { Component, PropTypes } from 'react';
-import { AppRegistry, Button, Navigator, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Animated, AppRegistry, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { requests } from 'util';
 
 /**
 * :D
-* TODO: Add additional screen or visibility/status options, animations
 * 
-* Parameters that you'll probably care about:
-*   query - The string that the user inputs (TODO: client-side validation)
-*/
+* States that you'll probably care about:
+*   query - The string that the user inputs
+*   results - The new string
+**/
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      data: [],
       text: '',
       query: '',
+      results: '',
+      offsetY: new Animated.Value(0),
     };
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={{height:40, textAlign:'center'}}
-            placeholder="Type here if you're a cool cat"
-            returnKeyLabel={"next"}
-            onChangeText={(text) => this.setState({query:text})}
-          />
-        </View>
+        <View style={styles.container}>
+          <Animated.View style={{transform: [{translateY: this.state.offsetY}]}}>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={{height:40, textAlign:'center'}}
+                placeholder="Enter a phrase or sentence"
+                returnKeyLabel={"next"}
+                onChangeText={(text) => this.setState({query:text})}
+              />
+            </View>
 
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            onPress={this._handleOnPress}
-            style={styles.buttonStyle}>
-              <Text style={styles.buttonTextStyle}>
-                Submit
-              </Text>
-          </TouchableOpacity>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                onPress={this._handleOnPress}
+                style={styles.buttonStyle}>
+                  <Text style={styles.buttonTextStyle}>
+                    Show me the magic
+                  </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.resultContainer}>
+              <TextInput
+                style={{textAlign:'center'}}
+                multiline={true}
+                numberOfLines={7}
+                editable={false}
+                value={this.state.results}
+                underlineColorAndroid="transparent"/>
+            </View>
+          </Animated.View>
         </View>
-      </View>
     );
   }
 
   _handleOnPress = () => {
+    // Log the user's input
     console.log(this.state.query);
+    this.setState({
+      results: this.state.query
+    });
+    Animated.timing(
+      this.state.offsetY,
+      { toValue: -100 }
+    ).start();
   };
 }
 
@@ -54,13 +77,17 @@ const styles = StyleSheet.create({
     flex:1,
     flexDirection:'column',
     justifyContent:'center',
-    backgroundColor: '#fff',
+    backgroundColor:'#fff',
   },
   inputContainer: {
     alignItems:'stretch',
   },
   buttonContainer: {
     alignItems:'center',
+  },
+  resultContainer: {
+    paddingTop:100,
+    alignItems:'stretch',
   },
   buttonStyle: {
     paddingVertical:15,
